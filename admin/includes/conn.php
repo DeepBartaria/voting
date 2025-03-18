f366b512bd23484b65bcbcc72c323272ce1b08b6
Deep Bartaria <80728476+DeepBartaria@users.noreply.github.com>
2025-03-18 11:38:28 +0530

    Update conn.php

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -1,8 +1,15 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'votesystem');
+$servername = "yanshuptl.mysql.database.azure.com";
+$username = "username@yanshuptl";
+$password = "";
+$dbname = "votesystem";
+$port = 3306;
 
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	}
-	
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
